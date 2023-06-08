f74b1e30b870a5bb4e4495595879b9629386aeff
Alex Parshyn <aleksandr.p@kyla.com>
2023-06-08 03:21:49 +0300

    finished 1 lab

---
+++ facade-service/src/main/java/ds/FacadeController.java
@@ -0,0 +1,50 @@
+package ds;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/facade")
+@Slf4j
+public class FacadeController {
+
+    WebClient loggingWebClient = WebClient.create("http://localhost:8081");
+    WebClient messagesWebClient = WebClient.create("http://localhost:8082");
+
+    @GetMapping()
+    public String getMessages() {
+        var loggingResponse = loggingWebClient
+                .get()
+                .uri("/logging")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        var messageResponse = messagesWebClient
+                .get()
+                .uri("/message")
+                .retrieve()
+                .bodyToMono(String.class)
+                .block();
+        return loggingResponse + " : " + messageResponse;
+    }
+
+    @PostMapping()
+    public void writeMessage(@RequestBody String body) {
+        var msg = new Message(UUID.randomUUID(), body);
+        log.info("Message {}", msg);
+        loggingWebClient.post()
+                .uri("/logging")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(Mono.just(msg), Message.class)
+                .retrieve()
+                .bodyToMono(Void.class)
+                .block();
+
+    }
+
+}
---
+++ model/src/main/java/ds/Message.java
@@ -0,0 +1,16 @@
+package ds;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Message {
+    private UUID id;
+    private String text;
+
+}
